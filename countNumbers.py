df54d54bc1f94f4a2ccbe478892793bfecba45ca
MichaelShapira <michaelshapira@hotmail.com>
2020-11-14 10:21:30 +0200

    change to the py file

--- countNumbers.py
+++ countNumbers.py
@@ -4,12 +4,11 @@ def main():
     num = 10
     for i in range(num):
         print(i + 1)
-        print(i * i)
-        # added in github
-        print(i + 5)
         # added in github again
         print(num - i)
         x = i  + x
+        print("this is a mistake code that will be reverted latter")
+    print("END!")
 if __name__ == "__main__":
     main()
 
